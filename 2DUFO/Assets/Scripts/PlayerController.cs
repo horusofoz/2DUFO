50d581d492ece54057011a58999ee423c0d4c62d
Jeff <horusofoz@gmail.com>
2018-05-04 23:03:12 +0930

    * Set pickup object colliders as trigger * Added kinemtaic rigidbody2d so unity sees these as dynamic colliders

--- 2DUFO/Assets/Scripts/PlayerController.cs
+++ 2DUFO/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,37 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rigidBody;
     [SerializeField] float playerSpeed = 10;
+    private int score = 0;
 
     private void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
-    
 
-    void FixedUpdate ()
+
+    void FixedUpdate()
+    {
+        ProcessMovement();
+    }
+
+    private void ProcessMovement()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rigidBody.AddForce(movement * playerSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PickUp"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateScore()
+    {
+
     }
 }
