d3a87e63f769463174534943277a6c09df20a7a4
Jeff <horusofoz@gmail.com>
2018-05-05 01:17:35 +0930

    * Set scoring * Set timer * Set win condition * Added start screen * Added restart function

--- 2DUFO/Assets/Scripts/PlayerController.cs
+++ 2DUFO/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rigidBody;
     [SerializeField] float playerSpeed = 10;
-    private int score = 0;
+    
+    private float score = 0;
+    public Text scoreText;
+    private float levelTimer = 20;
+    public Text levelTimerText;
+    public Text gameOverText;
+
+    private bool gameActive = true;
+
+    int pickupCount;
 
     private void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        pickupCount = GameObject.Find("Pickups").transform.childCount;
     }
 
 
     void FixedUpdate()
     {
-        ProcessMovement();
+        if(gameActive == true)
+        {
+            ProcessMovement();
+            UpdateTimer();
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(gameActive == false)
+        {
+            RestartGame();
+        }
     }
 
     private void ProcessMovement()
@@ -31,12 +56,55 @@ public class PlayerController : MonoBehaviour {
     {
         if(collision.CompareTag("PickUp"))
         {
-            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+            UpdateScore();
         }
     }
 
     private void UpdateScore()
     {
+        score++;
+        scoreText.text = "SCORE: " + score;
 
+        if (score == pickupCount)
+        {
+            EndGame();
+        }
+    }
+
+    private void UpdateTimer()
+    {
+        levelTimer -= Time.deltaTime;
+        levelTimerText.text = "TIME: " + Mathf.Round(levelTimer).ToString("00");
+        if(levelTimer < 0)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        gameActive = false;
+
+        if (levelTimer > 0)
+        {
+            score *= Mathf.Round(levelTimer);
+        }
+
+        scoreText.gameObject.SetActive(false);
+        levelTimerText.gameObject.SetActive(false);
+
+        gameOverText.text = "GAME OVER\n\n\n\n" + "FINAL SCORE: " + score + "\n\n\n\nPress Enter to start again";
+        gameOverText.gameObject.SetActive(true);
+
+        rigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void RestartGame()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
---
+++ 2DUFO/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
