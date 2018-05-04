e51a627caeba8012f6a9ac233c7003d16c99ee2b
Jeff <horusofoz@gmail.com>
2018-05-04 21:19:28 +0930

    * Put Rigidbody 2D on player gameObject * Created script for player movement * Completed lecture 3

---
+++ 2DUFO/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigidBody;
+    [SerializeField] float playerSpeed = 10;
+
+    private void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidBody.AddForce(movement * playerSpeed);
+    }
+}
